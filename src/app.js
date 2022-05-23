517275f21ea5d3ee55ae486d6e4b540bba32ad6c
Slals <jonathan@blocs.fr>
2022-05-23 02:12:42 +0200

    Reverts last commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
-require("dotenv").config({
-    path: '../.env',
-});
+require("dotenv").config();
 const LogLevel = require("loglevel");
 const useMdw = require("node-telegram-bot-api-middleware").use;
 const QRCode = require("qrcode");
