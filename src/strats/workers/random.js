630f6150dccd6fbe89e1bea7cc8b81793568e244
Slals <jonathan@blocs.fr>
2022-05-26 15:03:38 +0200

    Fixes random interval

--- src/strats/workers/random.js
+++ src/strats/workers/random.js
@@ -12,8 +12,7 @@ const Messages = {
 
 const options = workerData.opts;
 const lnmClient = new LNMarketsRest(workerData.lnmClient);
-// const callFrequencyInMs = 21 * 60 * 1000; // 21 minutes
-const callFrequencyInMs = 10000; // 21 minutes
+const callFrequencyInMs = 21 * 60 * 1000; // 21 minutes
 
 let createdPositions = [];
 let toRecursiveCall;
